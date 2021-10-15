2dfbe223ccc866cb8ca2101472b4320c8c884e3a
Jaeho Lee <jaeholee@upstage.ai>
2021-10-15 00:14:48 -0700

    add a port parameter
    ### TEST
    ```
    root@71b2324b9c87:/src# go run clogs.go
    NAME:
       clogs - log messages for a snorkel consumption
    
    USAGE:
       clogs [global options] command [command options] [arguments...]
    
    COMMANDS:
       help, h  Shows a list of commands or help for one command
    
    GLOBAL OPTIONS:
       --port number  a port number (default: 0)
       --help, -h     show help (default: false)
    clogs.go:33: "msg"="errors from app" "error"="Required flag "port" not set"
    root@71b2324b9c87:/src# go run clogs.go --port 3333
    clogs.go:25: "level"=0 "msg"="clogs" "port"=3333
    root@71b2324b9c87:/src# 
    ```

--- infra/services/clogs/clogs.go
+++ infra/services/clogs/clogs.go
@@ -13,8 +13,16 @@ var (
 	app    = &cli.App{
 		Name:  "clogs",
 		Usage: "log messages for a snorkel consumption",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:     "port",
+				Usage:    "a port `number`",
+				Required: true,
+			},
+		},
 		Action: func(c *cli.Context) error {
-			logger.Info("Hello CLogs!")
+			port := c.Int("port")
+			logger.Info("clogs", "port", port)
 			return nil
 		},
 	}
