01235b185c1f5f4a647fc42a2e0fb954019ac9fe
Jaeho Lee <jaeholee@upstage.ai>
2021-10-15 12:10:05 -0700

    [clogs] add a write function to Snorkel

--- infra/services/clogs/snorkel.go
+++ infra/services/clogs/snorkel.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
 const (
 	IntType = 1
 	StrType = 2
 	SetType = 3
 )
+
+type event struct {
+	Table string                 `json:"table"`
+	Token string                 `json:"token"`
+	Data  map[string]interface{} `json:"data"`
+}
 
 type Snorkel struct {
 	table string
@@ -35,4 +47,61 @@ func (s *Snorkel) AddSetField(name string) *Snorkel {
 func (s *Snorkel) addField(name string, fieldType int) *Snorkel {
 	s.spec[name] = fieldType
 	return s
+}
+
+func (s *Snorkel) Write(writer io.Writer, data map[string]interface{}) error {
+	out := map[string]interface{}{}
+	for k, v := range data {
+		ftype, ok := s.spec[k]
+		if !ok {
+			continue
+		}
+		switch ftype {
+		case IntType:
+			vv, ok := v.(int)
+			if !ok {
+				return fmt.Errorf("int type expected, %v", vv)
+			}
+			out[k] = v
+		case StrType:
+			vv, ok := v.(string)
+			if !ok {
+				return fmt.Errorf("string type expected, %v", vv)
+			}
+			out[k] = v
+		case SetType:
+			vv, ok := v.([]string)
+			if !ok {
+				return fmt.Errorf("expected an array of string type, but %v", vv)
+			}
+			out[k] = v
+		default:
+			return fmt.Errorf("unknown type is given, type=%v", ftype)
+		}
+	}
+
+	evt := event{
+		Table: s.table,
+		Token: s.token,
+		Data:  out,
+	}
+
+	bs := toJson(evt)
+	if nwritten, err := writer.Write(bs); err != nil {
+		return err
+	} else if nwritten != len(bs) {
+		return fmt.Errorf(
+			"failed to write all data, written=%v, expected=%v",
+			nwritten,
+			len(bs))
+	}
+	return nil
+}
+
+func toJson(e event) []byte {
+	bs, err := json.Marshal(e)
+	if err != nil {
+		panic(err)
+	}
+	return bs
 }
--- infra/services/clogs/snorkel_test.go
+++ infra/services/clogs/snorkel_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,5 +36,109 @@ func TestSpec(t *testing.T) {
 		ftype, ok := s.spec["categories"]
 		require.Equal(t, ftype, SetType)
 		require.True(t, ok)
+	})
+}
+
+func TestSnorkelWrite(t *testing.T) {
+	t.Run("should write integer fields", func(t *testing.T) {
+		s := NewSnorkel("hello", "__s__")
+		s.AddIntField("bytes")
+
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+		s.Write(w, map[string]interface{}{
+			"bytes": 1,
+		})
+		w.Flush()
+		assert.Equal(t, `{"table":"hello","token":"__s__","data":{"bytes":1}}`, b.String())
+	})
+	t.Run("should write string fields", func(t *testing.T) {
+		s := NewSnorkel("hello", "__s__")
+		s.AddStrField("id")
+
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+		s.Write(w, map[string]interface{}{
+			"id": "s00",
+		})
+		w.Flush()
+		assert.Equal(t, `{"table":"hello","token":"__s__","data":{"id":"s00"}}`, b.String())
+	})
+	t.Run("should write set fields", func(t *testing.T) {
+		s := NewSnorkel("hello", "__s__")
+		s.AddSetField("categories")
+
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+		s.Write(w, map[string]interface{}{
+			"categories": []string{"fruits", "books"},
+		})
+		w.Flush()
+		assert.Equal(t, `{"table":"hello","token":"__s__","data":{"categories":["fruits","books"]}}`, b.String())
+	})
+	t.Run("type mismatches", func(t *testing.T) {
+		t.Run("when int fields get other than integers", func(t *testing.T) {
+			s := NewSnorkel("hello", "__s__")
+			s.AddIntField("bytes")
+
+			badValues := []interface{}{nil, "300", 45.0, true, []interface{}{}}
+			for _, bad := range badValues {
+				t.Run(fmt.Sprintf("bad case: %v", bad), func(t *testing.T) {
+					var b bytes.Buffer
+					w := bufio.NewWriter(&b)
+					err := s.Write(w, map[string]interface{}{"bytes": bad})
+					assert.Error(t, err)
+				})
+			}
+		})
+		t.Run("when string fields get other than string values", func(t *testing.T) {
+			s := NewSnorkel("hello", "__s__")
+			s.AddStrField("id")
+
+			badValues := []interface{}{nil, 300, 45.0, true, []interface{}{}}
+			for _, bad := range badValues {
+				t.Run(fmt.Sprintf("bad case: %v", bad), func(t *testing.T) {
+					var b bytes.Buffer
+					w := bufio.NewWriter(&b)
+					err := s.Write(w, map[string]interface{}{"id": bad})
+					assert.Error(t, err)
+				})
+			}
+		})
+		t.Run("when set fields get other than an array of string", func(t *testing.T) {
+			s := NewSnorkel("hello", "__s__")
+			s.AddSetField("categories")
+
+			badValues := []interface{}{nil, 300, 45.0, true, []int{}, "cat"}
+			for _, bad := range badValues {
+				t.Run(fmt.Sprintf("bad case: %v", bad), func(t *testing.T) {
+					var b bytes.Buffer
+					w := bufio.NewWriter(&b)
+					err := s.Write(w, map[string]interface{}{"categories": bad})
+					assert.Error(t, err)
+				})
+			}
+		})
+	})
+	t.Run("should filter out unregistered fields", func(t *testing.T) {
+		s := NewSnorkel("hello", "__s__")
+		s.
+			AddIntField("bytes").
+			AddStrField("id").
+			AddSetField("categories")
+
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+		s.Write(w, map[string]interface{}{
+			"bytes":      10,
+			"id":         "abc",
+			"categories": []string{"fruits", "books"},
+			"source":     "http://example.com",
+		})
+		w.Flush()
+		assert.Equal(
+			t,
+			`{"table":"hello","token":"__s__","data":{"bytes":10,"categories":["fruits","books"],"id":"abc"}}`,
+			b.String())
 	})
 }
