b636632dfcd544f321141d3b78d5830ca7cefc6e
Jaeho Lee <jaeholee@upstage.ai>
2021-10-15 00:12:30 -0700

    add a cli app

--- infra/services/clogs/clogs.go
+++ infra/services/clogs/clogs.go
@@ -5,12 +5,23 @@ import (
 	"os"
 
 	"github.com/go-logr/stdr"
+	"github.com/urfave/cli/v2"
 )
 
 var (
 	logger = stdr.New(log.New(os.Stdout, "", log.Lshortfile))
+	app    = &cli.App{
+		Name:  "clogs",
+		Usage: "log messages for a snorkel consumption",
+		Action: func(c *cli.Context) error {
+			logger.Info("Hello CLogs!")
+			return nil
+		},
+	}
 )
 
 func main() {
-	logger.Info("Hello Clogs!")
+	if err := app.Run(os.Args); err != nil {
+		logger.Error(err, "errors from app")
+	}
 }
