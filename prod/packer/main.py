f94e25804c59fbc00469f2dafdee7f9d2f9aad23
Jaeho Lee <jaeholee@upstage.ai>
2021-10-22 20:57:35 -0700

    [packer] connection to the couch db
    Make a connection to the couchdb server. 
    Then grab the database, `packer`, to use later.

--- prod/packer/main.py
+++ prod/packer/main.py
@@ -1,15 +1,25 @@
 from typing import Optional
 from fastapi import FastAPI
+import couchdb
+import os
 
+DB_NAME = "packer"
+
+
+def connect_db(*, url: str):
+    server = couchdb.Server(url)
+    return server[DB_NAME] if DB_NAME in server else server.create(DB_NAME)
+
+
+db = connect_db(url=os.environ.get('DB_URL'))
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
-  return {"Hello": "World"}
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-  return {"item_id": item_id, "q": q}
-
-
-
+    return {"item_id": item_id, "q": q}
